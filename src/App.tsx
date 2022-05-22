f0a69726babf9bf83e64521717b1151c74c12f1e
Tor Arve Stangeland <tor.stangeland@bouvet.no>
2022-05-22 21:52:53 +0200

    Remove extra content on page.

--- src/App.tsx
+++ src/App.tsx
@@ -19,17 +19,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <p>Logged in as { instance.getActiveAccount()?.username || 'none' }</p>
           <button
             className="App-link"
