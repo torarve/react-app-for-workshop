102a6e810f544061dbbd0c7d8e72629b635b0c44
Tor Arve Stangeland <tor.stangeland@bouvet.no>
2022-05-22 21:27:36 +0200

    Add authentication.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MsalAuthenticationTemplate } from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
 
 function App() {
+  const authRequest = {
+    scopes: ["openid", "profile"]
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MsalAuthenticationTemplate
+      interactionType={InteractionType.Redirect}
+      authenticationRequest={authRequest}
+    >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </MsalAuthenticationTemplate>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { MsalProvider } from '@azure/msal-react';
+import { Configuration, PublicClientApplication } from '@azure/msal-browser';
+
+const configuration: Configuration = {
+  auth: {
+    clientId: '1caeec3d-ac26-4bcd-b18c-9983f34b9525',
+    authority: 'https://login.microsoftonline.com/7e793449-c4b5-4520-9737-0dcf3b24a192',
+    redirectUri: 'http://localhost:3000'
+  },
+}
+
+const pca = new PublicClientApplication(configuration);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <MsalProvider instance={pca}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MsalProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
