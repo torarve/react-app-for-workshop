fa71cc523f00988ec47d30f7abc21b1312cc60b5
Tor Arve Stangeland <tor.stangeland@bouvet.no>
2022-05-22 22:45:21 +0200

    Load user information from MS Graph API.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { MsalAuthenticationTemplate, useMsal } from '@azure/msal-react';
-import { InteractionType } from '@azure/msal-browser';
+import { AccountInfo, InteractionRequiredAuthError, InteractionStatus, InteractionType, IPublicClientApplication } from '@azure/msal-browser';
 
-function App() {
-  const authRequest = {
-    scopes: ["openid", "profile"]
+
+const authRequest = {
+  scopes: ["User.Read"],
+};
+
+
+type GraphData = {
+  businessPhones: string[];
+  displayName: string;
+  givenName: string;
+  jobTitle: string;
+  mail: string;
+  mobilePhone: string;
+  officeLocation: string;
+  preferredLanguage: string;
+  surname: string;
+  userPrincipalName: string;
+  id: string;
+}
+
+const me_url = 'https://graph.microsoft.com/v1.0/me/';
+const me_photo_url = 'https://graph.microsoft.com/v1.0/me/photo/$value';
+
+async function callMsGraph(url: string, msalInstance: IPublicClientApplication) {
+  const account = msalInstance.getActiveAccount();
+  if (!account) {
+      throw Error("No active account! Verify a user has been signed in and setActiveAccount has been called.");
+  }
+
+  const response = await msalInstance.acquireTokenSilent({
+      ...authRequest,
+      account: account
+  });
+
+  const headers = new Headers();
+  const bearer = `Bearer ${response.accessToken}`;
+
+  headers.append("Authorization", bearer);
+
+  const options = {
+      method: "GET",
+      headers: headers
   };
 
-  const { instance } = useMsal();
+  return await fetch(url, options);
+}
+
+async function getMe(msalInstance: IPublicClientApplication) {
+  const response = await callMsGraph(me_url, msalInstance);
+  return await response.json();
+}
+
+async function getPhoto(msalInstance: IPublicClientApplication) {
+  const response = await callMsGraph(me_photo_url, msalInstance);
+  const blob = await response.blob();
+  return URL.createObjectURL(blob);
+}
+
+
+function App() {
+  const { instance, inProgress } = useMsal();
+  const [ graphData, setGraphData ] = useState<GraphData | null>(null);
+  const [ photo, setPhoto ] = useState<string | null>(null);
+
+  useEffect(() => {
+    const acquireToken = () => {
+      instance.acquireTokenRedirect({
+        ...authRequest,
+        account: instance.getActiveAccount() as AccountInfo
+      });
+    }
+    if (!graphData && inProgress=== InteractionStatus.None) {
+      getMe(instance)
+        .then(setGraphData)
+        .catch((e) => { if (e instanceof InteractionRequiredAuthError) acquireToken(); });
+      getPhoto(instance)
+        .then(setPhoto)
+        .catch((e) => { if (e instanceof InteractionRequiredAuthError) acquireToken(); });
+    }
+  }, [instance, graphData, photo, inProgress])
+
+  useEffect(() => {
+    console.log(graphData)
+  }, [graphData]);
 
   return (
     <MsalAuthenticationTemplate
@@ -18,7 +96,7 @@ function App() {
     >
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={photo || logo} className="App-logo" alt="currently logged in user" />
           <p>Logged in as { instance.getActiveAccount()?.username || 'none' }</p>
           <button
             className="App-link"
